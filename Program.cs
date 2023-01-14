19322f4bd156de598a884f6e72f18e0dce128add
Valentisa11 <valya.kobylina@yandex.ru>
2023-01-14 23:28:09 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Module_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>//список пользователей
+            {
+                new User { Login = "login11", Name = "Миша Браун", IsPremium = true },
+                new User { Login = "MrSkobka", Name = "Сергей Костин", IsPremium = true },
+                new User { Login = "Pool", Name = "Mark Grin",  IsPremium = false}
+            };
+
+
+            Console.Write("Для входа в систему введите ваш логин: ");
+            string login = Console.ReadLine();//ввод логина
+
+            foreach (User u in users)
+            {
+                if (u.Login == login)
+                {
+                    Console.WriteLine($"Приветствуем, {u.Name}");//вывод приветствия с именем
+                    if (u.IsPremium == false)
+                    {
+                        User.ShowAds();
+                    }
+                }
+            }
+        }
+    }
+}
